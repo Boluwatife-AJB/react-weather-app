4ce2bc201b9fdd15e030ccd00c44837fcc1a93cf
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-07-22 04:45:29 +0100

    wonderful changes

--- src/api.js
+++ src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = '7c583d6533fe3d61a35ae29c4b682d72';
