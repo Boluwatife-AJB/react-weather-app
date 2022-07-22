1228c2ab36fc0f5c8937237247d8b4744035e7e4
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-07-22 14:28:51 +0100

    cities changes

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 export const geoApiOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'd08c69e912msh8a63cca67fd80bdp1804fdjsna08d6952cb7c',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-  },
+  		method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd08c69e912msh8a63cca67fd80bdp1804fdjsna08d6952cb7c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
