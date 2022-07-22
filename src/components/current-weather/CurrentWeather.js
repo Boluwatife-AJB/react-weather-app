c8d4adc73dd66cbf6e5c0f6c831a8540c3804b10
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-07-22 03:26:54 +0100

    another set of changes

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -4,10 +4,36 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Belgrade</p>
-        <p className="weather-description">Rainy</p>
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Rainy</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/10d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/10d.png" />
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">10 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">12 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
